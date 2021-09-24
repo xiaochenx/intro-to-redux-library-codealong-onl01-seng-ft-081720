c0c53ce983f94f9bfed42c3de4b2fadce96650a0
Xiaochen Xu <xiaochex@gmail.com>
2021-09-23 23:37:22 -0400

    added Provider component, used Redux with react

--- src/index.js
+++ src/index.js
@@ -6,5 +6,17 @@ import { createStore } from 'redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+    shoppingListItemReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
